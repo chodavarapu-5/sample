acc3cc6cb1f1af8c272d9e63fda5fb0df7a114b0
sai kumar <chodavarapufamily5@gmail.com>
2026-07-03 12:56:45 +0530

    conflict!
    Updated variable values for addition calculations.

--- myapp.java
+++ myapp.java
@@ -1,6 +1,6 @@
  public class myapp {
    public static void main(string[] args) {
-	   int a=10, int b=20;
+	   int a=100000, int b=2022020202020202;
 	   system.out.println("addition is:"+(a+b));
 	   	   system.out.println("addition is:"+(a+b));
 	   system.out.println("addition is:"+(a+b));
