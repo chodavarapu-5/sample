74fdddd958aa79e054dc5b9440397d1fd7ded668
sai kumar <chodavarapufamily5@gmail.com>
2026-07-03 12:59:57 +0530

    del and add
    Corrected variable types and fixed print statements.

--- myapp.java
+++ myapp.java
@@ -1,9 +1,7 @@
  public class myapp {
    public static void main(string[] args) {
-	   int a=100000, int b=2022020202020202;
-	   system.out.println("addition is:"+(a+b));
+	   float a=100000, int b=2022020202020202;
 	   	   system.out.println("addition is:"+(a+b));
-	   system.out.println("addition is:"+(a+b));
 	   system.out.println("addition is:"+(a+b));
 	   system.out.println("addition is:"+(a+b));
 
